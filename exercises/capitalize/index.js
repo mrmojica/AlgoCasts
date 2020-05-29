b9e4f87e264262f74d82becf6da0a7f910b3e203
mrmojica <mrmojica510@gmail.com>
2020-05-29 14:32:23 -0700

    complete capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,34 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+// My initial solution:
+function capitalize(str) {
+  // toUpperCase()
+  // slice()
+  let words = [];
+
+  for (let word of str.split(" ")) {
+    words.push(word[0].toUpperCase() + word.slice(1));
+  }
+
+  return words.join(" ");
+}
+
+// function capitalize(str) {
+//   let result = str[0].toUpperCase();
+//   let i = 1;
+
+// can use a for loop instead
+//   while (i < str.length) {
+//     if (str[i - 1] === " ") {
+//       result += str[i].toUpperCase();
+//     } else {
+//       result += str[i];
+//     }
+//     i++;
+//   }
+
+//   return result;
+// }
 
 module.exports = capitalize;
