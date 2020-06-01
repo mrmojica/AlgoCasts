349e4cd0daef2742c16783dca030b5ca5cbfbed4
mrmojica <mrmojica510@gmail.com>
2020-06-01 11:50:20 -0700

    complete printing steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,72 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// function steps(n) {
+//   let counter = 0;
+//   let step = "";
+
+//   for (let i = 0; i < n; i++) {
+//     step = "#" + step;
+
+//     while (step.length < n) {
+//       step = step + " ";
+//     }
+
+//     console.log(step);
+//     step = "";
+//   }
+// }
+
+// notes:
+// dealing with a 2d array i (row), j (column)
+// From O to n (iterate through rows)
+//     Create an empty string, 'stair'
+//     From 0 to n
+//         IF the current column is equal to or less than the current row
+//             Add a '#' to 'stair'
+//         ELSE
+//             Add a space to 'stair'
+//         Console.log 'stair'
+// iterative solution
+function steps(n) {
+  for (let row = 0; row < n; row++) {
+    let stair = "";
+    for (let column = 0; column < n; column++) {
+      if (column <= row) {
+        stair = stair + "#";
+      } else {
+        stair = stair + " ";
+      }
+    }
+
+    console.log(stair);
+  }
+}
+
+// Recursion Tips:
+// If (row == n) then we have hit the end of our problem.
+// If the 'stair' has a length === n then we are at the end of row.
+// If the length of the stair string is less than or equal to the row number we're working on, we add a '#', otherwise add a space.
+// recursion solution
+// function steps(n, row = 0, stair = "") {
+//   if (n === row) {
+//     return;
+//   }
+
+//   // We no longer want to continue any code if the step is completed
+//   if (n === stair.length) {
+//     console.log(stair);
+//     return steps(n, row + 1);
+//   }
+
+//   // create the step recursively
+//   if (stair.length <= row) {
+//     stair += "#";
+//   } else {
+//     stair += " ";
+//   }
+
+//   steps(n, row, stair);
+// }
 
 module.exports = steps;
