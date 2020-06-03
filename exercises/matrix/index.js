7d3a59c7630b0dff3e1717d7ace5468000de47f8
mrmojica <mrmojica510@gmail.com>
2020-06-03 16:45:59 -0700

    complete matrix spirals

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,69 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+// Notes:
+// Create empty array called 'results'
+// Create a counter variable, starting at 1
+// As long as (start column <= end column) AND (start row <= end row)
+//  Loop from start column to end column
+//      As results[start_row][i] assign counter variable
+//      Increment counter
+//  Increment start row
+//  Loop from start row to end row
+//      At results[i][end_column] assign counter variable
+//      Increment counter
+//  Decrement end column
+//  ...repeat for two sides
+
+function matrix(n) {
+  const result = [];
+
+  for (let i = 0; i < n; i++) {
+    result.push([]);
+  }
+
+  let counter = 1;
+  let start_column = 0;
+  let end_column = n - 1;
+  let start_row = 0;
+  let end_row = n - 1;
+
+  while (start_column <= end_column && start_row <= end_row) {
+    // Top Row
+    for (let i = start_column; i <= end_column; i++) {
+      result[start_row][i] = counter;
+      counter++;
+    }
+
+    start_row++;
+
+    // Right Column
+    for (let i = start_row; i <= end_row; i++) {
+      result[i][end_column] = counter;
+      counter++;
+    }
+
+    end_column--;
+
+    // Bottom Row
+    for (let i = end_column; i >= start_column; i--) {
+      result[end_row][i] = counter;
+      counter++;
+    }
+
+    end_row--;
+
+    // Start Column
+    for (let i = end_row; i >= start_row; i--) {
+      result[i][start_column] = counter;
+      counter++;
+    }
+
+    start_column++;
+  }
+  console.log(result);
+
+  return result;
+}
 
 module.exports = matrix;
