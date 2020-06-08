46b704e0a88d876bfeb6a84ff5593ffb92be869b
mrmojica <mrmojica510@gmail.com>
2020-06-08 10:38:37 -0700

    complete queue

--- exercises/queue/index.js
+++ exercises/queue/index.js
@@ -8,6 +8,20 @@
 //     q.add(1);
 //     q.remove(); // returns 1;
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.data = [];
+  }
+
+  // Add
+  add(value) {
+    this.data.unshift(value);
+  }
+
+  // Remove
+  remove() {
+    return this.data.pop();
+  }
+}
 
 module.exports = Queue;
